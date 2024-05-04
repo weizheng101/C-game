7491889beacab2a1070ee96123687804e1201b95
weizhengjojo <2967969813@qq.com>
2024-05-04 14:21:13 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace geme
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
